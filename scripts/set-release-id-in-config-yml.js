223e20091ef28fc47fb2d1e3bc72153e2e86e30f
Carl-Erik Kopseng <carlerik@gmail.com>
2019-02-28 16:56:59 +0100

    Use only major version as current_release

--- scripts/set-release-id-in-config-yml.js
+++ scripts/set-release-id-in-config-yml.js
@@ -2,11 +2,13 @@
 var configYmlPath = "docs/_config.yml";
 var UTF8 = "utf8";
 
+var semver = require("semver");
 var fs = require("fs");
 var yaml = require("js-yaml");
-var releaseId = "v" + require("../package.json").version;
+var releaseId = require("../package.json").version;
+var majorVersion = semver.major(releaseId);
 var config = yaml.safeLoad(fs.readFileSync(configYmlPath, UTF8));
 
-config.sinon.current_release = releaseId; // eslint-disable-line camelcase
+config.sinon.current_release = "v" + majorVersion; // eslint-disable-line camelcase
 
 fs.writeFileSync(configYmlPath, yaml.safeDump(config), UTF8);
