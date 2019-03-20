a193d6d12a3d8d57af2ad3c475bb09c116292253
Carl-Erik Kopseng <carlerik@gmail.com>
2019-03-20 11:12:57 +0100

    Try out Circle CI's own version of the node:10 image - FAILS

--- run-tests.sh
+++ run-tests.sh
@@ -14,7 +14,7 @@ npm config set strict-ssl false
 #wget -O google-chrome-stable_v71.deb https://www.slimjet.com/chrome/download-chrome.php?file=files%2F71.0.3578.80%2Fgoogle-chrome-stable_current_amd64.deb
 #dpkg -i google-chrome-stable_v71.deb
 
-#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google.list
 apt-get -qq update
 apt-get -qq install -y --no-install-recommends google-chrome-stable
