c6302c74d841f7b165307377fb87d7f27edaef39
Carl-Erik Kopseng <carlerik@gmail.com>
2019-03-20 11:05:42 +0100

    Pinned this down to renderer crashing in Chrome v73

--- run-tests.sh
+++ run-tests.sh
@@ -8,17 +8,17 @@ SINON_CHROME_BIN=/usr/bin/google-chrome-unstable
 
 echo Setup
 npm config set strict-ssl false
-npm install # need to uncomment this when editing mochify to pass `dumpio: true`
+#npm install # need to uncomment this when editing mochify to pass `dumpio: true`
 
 # Download Chrome 71
-wget -O google-chrome-stable_v71.deb https://www.slimjet.com/chrome/download-chrome.php?file=files%2F71.0.3578.80%2Fgoogle-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_v71.deb
+#wget -O google-chrome-stable_v71.deb https://www.slimjet.com/chrome/download-chrome.php?file=files%2F71.0.3578.80%2Fgoogle-chrome-stable_current_amd64.deb
+#dpkg -i google-chrome-stable_v71.deb
 
 #wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-#echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google.list
-#apt-get -qq update
-#apt-get -qq install -y --no-install-recommends google-chrome-stable=72.0.3626.121
-#apt-get -qq install -y --no-install-recommends google-chrome-unstable
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google.list
+apt-get -qq update
+apt-get -qq install -y --no-install-recommends google-chrome-stable
+apt-get -qq install -y --no-install-recommends google-chrome-unstable
 
 echo Pre-Test
 # Hangs here after successfull test run
